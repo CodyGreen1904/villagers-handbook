28bb04d531d3dd6d1477f92bcc5796cc6a2473ec
mberz2 <mberz2@pdx.edu>
2022-02-14 18:51:10 -0800

    Working on button routing to main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Welcome() {
-  function handleClick(path) {
-    history.push(path);
-  }
-
   return (
     <div className="welcome">
       {/*CENTERED PAGE ENTRY*/}
       <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
         <h1>Title</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        <button class="btn btn-primary" onClick={() => handleClick("search")}>
-          Get Started
-        </button>
+
+        <Link to="/about">
+          <button class="btn btn-primary" to="/search">
+            Enter Site
+          </button>
+        </Link>
       </div>
       {/*END PAGE ENTRY*/}
     </div>
