92267c7fb50478dc001b1b1ee694a5f07265bbcc
mberz2 <mberz2@pdx.edu>
2022-02-14 16:08:08 -0800

    Added imports and exports for rendering of components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./components/App";
+
+//Routing imports
+import { Navigation, Footer, Home } from "./components";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Placeholder1 />} />
+      <Route path="/" element={<Placeholder2 />} />
+      <Route path="/" element={<Placeholder3 />}>
+        <Route path="" element={<Placeholder5 />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
