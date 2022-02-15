b0aa0170614acb24ac70595350b7f7033fc5e9de
mberz2 <mberz2@pdx.edu>
2022-02-14 22:18:56 -0800

    Fixing improper uses of class vs classname

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,22 @@ function Home() {
   return (
     <div className="home" id="welcome_page">
       {/*CENTERED PAGE ENTRY*/}
-      <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+      <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
         <h1>Title</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 
-        <Link to="/about">
-          <button class="btn btn-primary" to="/search">
-            Enter Site
-          </button>
-        </Link>
+        <div className="btn-group-vertical">
+          <div className="row row-margin-05">
+            <Link to="/search">
+              <button class="btn btn-primary">Enter Site</button>
+            </Link>
+          </div>
+          <div className="row row-margin-05">
+            <Link to="/about">
+              <button className="btn btn-primary">About Us</button>
+            </Link>
+          </div>
+        </div>
       </div>
       {/*END PAGE ENTRY*/}
     </div>
