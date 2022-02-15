f0b53f532cb5253c458313fd8edafd4b484f2e76
mberz2 <mberz2@pdx.edu>
2022-02-14 16:38:00 -0800

    Rename welcome to home

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //Routing imports
-import { Navigation, Footer, Welcome } from "./components";
+import { Navigation, Footer, Home } from "./components";
 
 ReactDOM.render(
   <Router>
     <Navigation />
     <Routes>
-      <Route path="/" element={<Welcome />} />
+      <Route path="/" element={<Home />} />
     </Routes>
     <Footer />
   </Router>
