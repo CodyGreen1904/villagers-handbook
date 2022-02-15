a6de139a207a084c1d1cbd399d89f206530b7617
mberz2 <mberz2@pdx.edu>
2022-02-14 16:23:55 -0800

    Updated styling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //Routing imports
-import { Navigation, Footer, Home } from "./components";
+import { Navigation, Footer, Welcome } from "./components";
 
 ReactDOM.render(
   <Router>
