2143179d3ab36d52d8fad487de89499c8d93af2d
mberz2 <mberz2@pdx.edu>
2022-02-14 18:22:35 -0800

    Creating welcome page layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 function Welcome() {
+  function handleClick(path) {
+    history.push(path);
+  }
+
   return (
     <div className="welcome">
       {/*CENTERED PAGE ENTRY*/}
-<div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-    <h1>Title</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    <button class="btn btn-primary">Get Started</button>
-</div>
-{/*END PAGE ENTRY*/}
+      <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button class="btn btn-primary" onClick={() => handleClick("search")}>
+          Get Started
+        </button>
+      </div>
+      {/*END PAGE ENTRY*/}
     </div>
   );
 }
