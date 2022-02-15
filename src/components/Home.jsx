416fc979e31e5ae78ccaceafed3227848f714c04
mberz2 <mberz2@pdx.edu>
2022-02-14 18:00:38 -0800

    Added navbar visibility only to inner pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,12 @@ import React from "react";
 function Welcome() {
   return (
     <div className="welcome">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
+      <div class="d-flex flex-column">
+        <div class="d-flex p-2">Welcome to Villager's Handbook</div>
+        <div class="p-2">Filler Text</div>
+        <button type="button" class="btn btn-primary">
+          Enter Site
+        </button>
       </div>
     </div>
   );
