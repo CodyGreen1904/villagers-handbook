773e6d63b18832a7fcffc572693dc05d1e069d75
mberz2 <mberz2@pdx.edu>
2022-02-14 16:42:52 -0800

    Added document render

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //Routing imports
-import { Navigation, Footer, Home } from "./components";
+import { Navigation, Footer, Home, About } from "./components";
 
 ReactDOM.render(
   <Router>
     <Navigation />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/" element={<About />} />
     </Routes>
     <Footer />
   </Router>,
