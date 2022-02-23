d91fb3769ca07bcd63f787507e512306f703a988
Sammi <samaki2@pdx.edu>
2022-02-23 13:31:22 -0800

    add styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navigation } from "../components";
+import "../styles/about.css";
 
 function About() {
   return (
-    <div className="about">
+    <div id="aboutDiv" className="about min-vh-100">
       <Navigation />
       <div className="container">
         <div className="row align-items-center my-5">
@@ -17,10 +18,7 @@ function About() {
           <div className="col-lg-5">
             <h1 className="font-weight-light">About</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              The Villager's Handook is a place to learn about all of the fascinating residents of Animal Crossing: New Horizons. 
             </p>
           </div>
         </div>
