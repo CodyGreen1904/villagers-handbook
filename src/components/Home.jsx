174ab9ff857875f2f69903221a6309332680b065
mberz2 <mberz2@pdx.edu>
2022-02-22 17:49:09 -0800

    Updates to welcome page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,39 +4,40 @@ import axios from "axios";
 import "../styles/welcome.css";
 
 function Home() {
+  const [post, setPost] = React.useState(null);
+
   let url = "http://acnhapi.com/v1/";
   let route = "villagers/";
   let id = "57";
 
-  const [post, setPost] = React.useState(null);
-
   React.useEffect(() => {
     axios.get(url + route + id).then((response) => {
       console.log(response.data);
+      setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
-
   return (
-    <div class="container col-xxl-8 px-4 py-5">
-      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div class="col-10 col-sm-8 col-lg-6">
-          <div class="villager_container d-block mx-lg-auto"></div>
-        </div>
-        <div class="col-lg-6">
-          <h1 class="display-5 fw-bold lh-1 mb-3">
-            Welcome to the Villagers Handbook
-          </h1>
-          <p class="lead">
-            Villagers Handbook is a lightweight search application that allows
-            you to quickly and easily look up information related to the world
-            of Animal Crossing New Horizons.
-          </p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">
-              Enter Site
-            </button>
+    <div class="welcome_page">
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <div className="villager_container d-block mx-lg-auto"></div>
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">
+              Welcome to the Villagers Handbook
+            </h1>
+            <p className="lead">
+              Villagers Handbook is a lightweight search application that allows
+              you to quickly and easily look up information related to the world
+              of Animal Crossing New Horizons.
+            </p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">
+                Enter Site
+              </button>
+            </div>
           </div>
         </div>
       </div>
