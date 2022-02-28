37eb2f8961ad0f7f740f871fe4dbb47ade6ced50
mberz2 <mberz2@pdx.edu>
2022-02-27 17:47:20 -0800

    updates to styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,7 +41,7 @@ export default function Home(props) {
   }, []);
 
   return (
-    <main>
+    <main id="homepage">
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-12 col-lg-6">
